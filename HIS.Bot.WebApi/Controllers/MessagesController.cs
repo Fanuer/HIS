c1a8f3f70133537def254430685f4cdad7fee735
Fanuer <stefan.suermann@gmail.com>
2017-01-10 21:38:08 +0100

    added ResourceFiles to Bot Addded Welcome-Message to Bot

--- HIS.Bot.WebApi/Controllers/MessagesController.cs
+++ HIS.Bot.WebApi/Controllers/MessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -48,31 +49,37 @@ namespace HIS.Bot.WebApi.Controllers
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                var reply = activity.CreateReply($"You sent {length} characters");
-                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                var peng = await connector.Conversations.ReplyToActivityWithHttpMessagesAsync(activity.Conversation.Id, activity.Id, reply, cancellationToken: token);
-                
+                await SendReplyMessage($"You sent {length} characters", activity, token);
             }
             else
             {
-                HandleSystemMessage(activity);
+                await HandleSystemMessage(activity, token);
             }
             return Ok();
         }
 
-        private IActionResult HandleSystemMessage(Activity activity)
+        private async Task HandleSystemMessage(Activity incoming, CancellationToken token)
         {
-            switch (activity.GetActivityType())
+            switch (incoming.GetActivityType())
             {
-                case ActivityTypes.Ping:                    // An activity sent to test the security of a bot.
-                    var reply = activity.CreateReply();
+                case ActivityTypes.Ping:                    // An incoming sent to test the security of a bot.
+                    var reply = incoming.CreateReply();
                     reply.Type = ActivityTypes.Ping;
-
-                    return Ok();
+                    await SendReplyMessage(incoming, reply, token);
                     break;
                 case ActivityTypes.ContactRelationUpdate:   // The bot was added to or removed from a user's contact list
+                    if (incoming.AsContactRelationUpdateActivity().Action.Equals("add"))
+                    {
+                        await SendReplyMessage(Resource.Message_Welcome, incoming, token);
+                    }
+
+                    await SendReplyMessage(Resource.Message_Welcome, incoming, token);
                     break;
                 case ActivityTypes.ConversationUpdate:      // This notification is sent when the conversation's properties change, for example the topic name, or when user joins or leaves the group
+                    if (incoming.AsConversationUpdateActivity().MembersAdded.Any())
+                    {
+                        await SendReplyMessage(Resource.Message_Welcome, incoming, token);
+                    }
                     break;
                 case ActivityTypes.DeleteUserData:          // A user has requested for the bot to delete any profile / user data
                     break;
@@ -83,8 +90,20 @@ namespace HIS.Bot.WebApi.Controllers
                 case ActivityTypes.Typing:                  // The user or bot on the other end of the conversation is typing
                     break;
             }
+        }
 
-            return null;
+        private async Task SendReplyMessage(string text, Activity activity, CancellationToken token = default(CancellationToken))
+        {
+            var reply = activity.CreateReply(text);
+            await SendReplyMessage(activity, reply, token);
+        }
+
+        private async Task SendReplyMessage(Activity incomingAct, Activity outgoingAct, CancellationToken token = default(CancellationToken))
+        {
+            using (var connector = new ConnectorClient(new Uri(incomingAct.ServiceUrl)))
+            {
+                await connector.Conversations.ReplyToActivityWithHttpMessagesAsync(incomingAct.Conversation.Id, incomingAct.Id, outgoingAct, cancellationToken: token);
+            }
         }
 
         #endregion
